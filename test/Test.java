2819bf441c3e055db26de7eb5c1a93a4fa23d27a
Wangwenzhuo <842988533@qq.com>
2018-10-07 11:32:51 +0800

    create +,-,*,/

--- test/Test.java
+++ test/Test.java
@@ -11,8 +11,14 @@ public class Test {
 		double number1 = input.nextDouble();
 		System.out.println("请输入第二个数：");
 		double number2 = input.nextDouble();
-		double number = number1 + number2;
-		System.out.println("两数之和为：" + number);
+		double number3 = number1 + number2;
+		double number4 = number1 - number2;
+		double number5 = number1 * number2;
+		double number6 = number1 / number2;
+		System.out.println("两数之和为：" + number3);
+		System.out.println("两数之差为：" + number4);
+		System.out.println("两数之积为：" + number5);
+		System.out.println("两数之商为：" + number6);
 	}
 
 }
